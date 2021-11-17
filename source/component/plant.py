2314a9a6c08dcbb717d73e5db93bac4af4394c82
Can I? <ljh15952@gmail.com>
2021-11-17 11:25:56 +0900

    Add plant x2 speed

--- source/component/plant.py
+++ source/component/plant.py
@@ -24,7 +24,7 @@ class Car(pg.sprite.Sprite):
         if self.state == c.IDLE:
             pass
         elif self.state == c.WALK:
-            self.rect.x += 4
+            self.rect.x += 4 * c.DELTA_TIME
         if self.rect.x > c.SCREEN_WIDTH:
             self.dead = True
 
@@ -87,14 +87,14 @@ class Bullet(pg.sprite.Sprite):
         self.current_time = game_info[c.CURRENT_TIME]
         if self.state == c.FLY:
             if self.rect.y != self.dest_y:
-                self.rect.y += self.y_vel
+                self.rect.y += self.y_vel * c.DELTA_TIME
                 if self.y_vel * (self.dest_y - self.rect.y) < 0:
                     self.rect.y = self.dest_y
-            self.rect.x += self.x_vel
+            self.rect.x += self.x_vel * c.DELTA_TIME
             if self.rect.x > c.SCREEN_WIDTH:
                 self.kill()
         elif self.state == c.EXPLODE:
-            if(self.current_time - self.explode_timer) > 500:
+            if(self.current_time - self.explode_timer) > 500 / c.DELTA_TIME:
                 self.kill()
 
     def setExplode(self):
@@ -125,7 +125,7 @@ class Plant(pg.sprite.Sprite):
         self.bullet_group = bullet_group
         self.can_sleep = False
         self.animate_timer = 0
-        self.animate_interval = 100
+        self.animate_interval = 100 / c.DELTA_TIME
         self.hit_timer = 0
 
     def loadFrames(self, frames, name, scale, color=c.BLACK):
@@ -187,7 +187,7 @@ class Plant(pg.sprite.Sprite):
             self.animate_timer = self.current_time
         
         self.image = self.frames[self.frame_index]
-        if(self.current_time - self.hit_timer) >= 200:
+        if(self.current_time - self.hit_timer) >= 200 / c.DELTA_TIME:
             self.image.set_alpha(255)
         else:
             self.image.set_alpha(192)
@@ -227,7 +227,7 @@ class Sun(Plant):
             scale = 0.6
             self.sun_value = 12
         Plant.__init__(self, x, y, c.SUN, 0, None, scale)
-        self.move_speed = 1
+        self.move_speed = 1 * c.DELTA_TIME
         self.dest_x = dest_x
         self.dest_y = dest_y
         self.die_timer = 0
@@ -241,7 +241,7 @@ class Sun(Plant):
         if self.rect.centerx == self.dest_x and self.rect.bottom == self.dest_y:
             if self.die_timer == 0:
                 self.die_timer = self.current_time
-            elif(self.current_time - self.die_timer) > c.SUN_LIVE_TIME:
+            elif(self.current_time - self.die_timer) > c.SUN_LIVE_TIME / c.DELTA_TIME:
                 self.state = c.DIE
                 self.kill()
 
@@ -264,7 +264,7 @@ class SunFlower(Plant):
     def idling(self):
         if self.sun_timer == 0:
             self.sun_timer = self.current_time - (c.FLOWER_SUN_INTERVAL - 6000)
-        elif (self.current_time - self.sun_timer) > c.FLOWER_SUN_INTERVAL:
+        elif (self.current_time - self.sun_timer) > c.FLOWER_SUN_INTERVAL / c.DELTA_TIME:
             self.sun_group.add(Sun(self.rect.centerx, self.rect.bottom, self.rect.right, self.rect.bottom + self.rect.h // 2))
             self.sun_timer = self.current_time
 
@@ -274,7 +274,7 @@ class PeaShooter(Plant):
         self.shoot_timer = 0
         
     def attacking(self):
-        if (self.current_time - self.shoot_timer) > 2000:
+        if (self.current_time - self.shoot_timer) > 2000 / c.DELTA_TIME:
             self.bullet_group.add(Bullet(self.rect.right, self.rect.y, self.rect.y,
                                     c.BULLET_PEA, c.BULLET_DAMAGE_NORMAL, False))
             self.shoot_timer = self.current_time
@@ -285,7 +285,7 @@ class RepeaterPea(Plant):
         self.shoot_timer = 0
 
     def attacking(self):
-        if (self.current_time - self.shoot_timer) > 2000:
+        if (self.current_time - self.shoot_timer) > 2000 / c.DELTA_TIME:
             self.bullet_group.add(Bullet(self.rect.right, self.rect.y, self.rect.y,
                                     c.BULLET_PEA, c.BULLET_DAMAGE_NORMAL, False))
             self.bullet_group.add(Bullet(self.rect.right + 40, self.rect.y, self.rect.y,
@@ -300,7 +300,7 @@ class ThreePeaShooter(Plant):
         self.bullet_groups = bullet_groups
 
     def attacking(self):
-        if (self.current_time - self.shoot_timer) > 2000:
+        if (self.current_time - self.shoot_timer) > 2000 / c.DELTA_TIME:
             offset_y = 9 # modify bullet in the same y position with bullets of other plants
             for i in range(3):
                 tmp_y = self.map_y + (i - 1)
@@ -317,7 +317,7 @@ class SnowPeaShooter(Plant):
         self.shoot_timer = 0
 
     def attacking(self):
-        if (self.current_time - self.shoot_timer) > 2000:
+        if (self.current_time - self.shoot_timer) > 2000 / c.DELTA_TIME:
             self.bullet_group.add(Bullet(self.rect.right, self.rect.y, self.rect.y,
                                     c.BULLET_PEA_ICE, c.BULLET_DAMAGE_NORMAL, True))
             self.shoot_timer = self.current_time
@@ -373,10 +373,10 @@ class CherryBomb(Plant):
         if self.start_boom:
             if self.bomb_timer == 0:
                 self.bomb_timer = self.current_time
-            elif(self.current_time - self.bomb_timer) > 500:
+            elif(self.current_time - self.bomb_timer) > 500 / c.DELTA_TIME:
                 self.health = 0
         else:
-            if (self.current_time - self.animate_timer) > 100:
+            if (self.current_time - self.animate_timer) > 100 / c.DELTA_TIME:
                 self.frame_index += 1
                 if self.frame_index >= self.frame_num:
                     self.setBoom()
@@ -388,9 +388,9 @@ class CherryBomb(Plant):
 class Chomper(Plant):
     def __init__(self, x, y):
         Plant.__init__(self, x, y, c.CHOMPER, c.PLANT_HEALTH, None)
-        self.animate_interval = 250
+        self.animate_interval = 250 / c.DELTA_TIME
         self.digest_timer = 0
-        self.digest_interval = 15000
+        self.digest_interval = 15000 / c.DELTA_TIME
         self.attack_zombie = None
         self.zombie_group = None
 
@@ -470,7 +470,7 @@ class PuffShroom(Plant):
         self.frames = self.idle_frames
 
     def attacking(self):
-        if (self.current_time - self.shoot_timer) > 3000:
+        if (self.current_time - self.shoot_timer) > 3000 / c.DELTA_TIME:
             self.bullet_group.add(Bullet(self.rect.right, self.rect.y + 10, self.rect.y + 10,
                                     c.BULLET_MUSHROOM, c.BULLET_DAMAGE_NORMAL, True))
             self.shoot_timer = self.current_time
@@ -484,7 +484,7 @@ class PuffShroom(Plant):
 class PotatoMine(Plant):
     def __init__(self, x, y):
         Plant.__init__(self, x, y, c.POTATOMINE, c.PLANT_HEALTH, None)
-        self.animate_interval = 300
+        self.animate_interval = 300 / c.DELTA_TIME
         self.is_init = True
         self.init_timer = 0
         self.bomb_timer = 0
@@ -512,7 +512,7 @@ class PotatoMine(Plant):
         if self.is_init:
             if self.init_timer == 0:
                 self.init_timer = self.current_time
-            elif (self.current_time - self.init_timer) > 15000:
+            elif (self.current_time - self.init_timer) > 15000 / c.DELTA_TIME:
                 self.changeFrames(self.idle_frames)
                 self.is_init = False
 
@@ -526,7 +526,7 @@ class PotatoMine(Plant):
         if self.bomb_timer == 0:
             self.bomb_timer = self.current_time
             self.changeFrames(self.explode_frames)
-        elif (self.current_time - self.bomb_timer) > 500:
+        elif (self.current_time - self.bomb_timer) > 500 / c.DELTA_TIME:
             self.health = 0
 
 class Squash(Plant):
@@ -574,11 +574,11 @@ class Squash(Plant):
         elif self.aim_timer == 0:
             self.aim_timer = self.current_time
             self.changeFrames(self.aim_frames)
-        elif (self.current_time - self.aim_timer) > 1000:
+        elif (self.current_time - self.aim_timer) > 1000 / c.DELTA_TIME:
             self.changeFrames(self.attack_frames)
             self.rect.centerx = self.attack_zombie.rect.centerx
             self.squashing = True
-            self.animate_interval = 300
+            self.animate_interval = 300 / c.DELTA_TIME
 
     def getPosition(self):
         return self.orig_pos
@@ -586,7 +586,7 @@ class Squash(Plant):
 class Spikeweed(Plant):
     def __init__(self, x, y):
         Plant.__init__(self, x, y, c.SPIKEWEED, c.PLANT_HEALTH, None)
-        self.animate_interval = 200
+        self.animate_interval = 200 / c.DELTA_TIME
         self.attack_timer = 0
 
     def loadImages(self, name, scale):
@@ -594,7 +594,7 @@ class Spikeweed(Plant):
 
     def setIdle(self):
         print('spikeweed idle')
-        self.animate_interval = 200
+        self.animate_interval = 200 / c.DELTA_TIME
         self.state = c.IDLE
 
     def canAttack(self, zombie):
@@ -609,7 +609,7 @@ class Spikeweed(Plant):
         self.state = c.ATTACK
 
     def attacking(self):
-        if (self.current_time - self.attack_timer) > 2000:
+        if (self.current_time - self.attack_timer) > 2000 / c.DELTA_TIME:
             self.attack_timer = self.current_time
             for zombie in self.zombie_group:
                 if self.canAttack(zombie):
@@ -639,14 +639,14 @@ class Jalapeno(Plant):
 
     def animation(self):
         if self.start_explode:
-            if(self.current_time - self.animate_timer) > 100:
+            if(self.current_time - self.animate_timer) > 100 / c.DELTA_TIME:
                 self.frame_index += 1
                 if self.frame_index >= self.frame_num:
                     self.health = 0
                     return
                 self.animate_timer = self.current_time
         else:
-            if (self.current_time - self.animate_timer) > 100:
+            if (self.current_time - self.animate_timer) > 100 / c.DELTA_TIME:
                 self.frame_index += 1
                 if self.frame_index >= self.frame_num:
                     self.setExplode()
@@ -700,7 +700,7 @@ class ScaredyShroom(Plant):
         self.changeFrames(self.idle_frames)
 
     def attacking(self):
-        if (self.current_time - self.shoot_timer) > 2000:
+        if (self.current_time - self.shoot_timer) > 2000 / c.DELTA_TIME:
             self.bullet_group.add(Bullet(self.rect.right, self.rect.y + 40, self.rect.y + 40,
                                     c.BULLET_MUSHROOM, c.BULLET_DAMAGE_NORMAL, True))
             self.shoot_timer = self.current_time
@@ -709,7 +709,7 @@ class SunShroom(Plant):
     def __init__(self, x, y, sun_group):
         Plant.__init__(self, x, y, c.SUNSHROOM, c.PLANT_HEALTH, None)
         self.can_sleep = True
-        self.animate_interval = 200
+        self.animate_interval = 200 / c.DELTA_TIME
         self.sun_timer = 0
         self.sun_group = sun_group
         self.is_big = False
@@ -736,13 +736,13 @@ class SunShroom(Plant):
         if not self.is_big:
             if self.change_timer == 0:
                 self.change_timer = self.current_time
-            elif (self.current_time - self.change_timer) > 25000:
+            elif (self.current_time - self.change_timer) > 25000 / c.DELTA_TIME:
                 self.changeFrames(self.big_frames)
                 self.is_big = True
         
         if self.sun_timer == 0:
             self.sun_timer = self.current_time - (c.FLOWER_SUN_INTERVAL - 6000)
-        elif (self.current_time - self.sun_timer) > c.FLOWER_SUN_INTERVAL:
+        elif (self.current_time - self.sun_timer) > c.FLOWER_SUN_INTERVAL / c.DELTA_TIME:
             self.sun_group.add(Sun(self.rect.centerx, self.rect.bottom, self.rect.right,
                                    self.rect.bottom + self.rect.h // 2, self.is_big))
             self.sun_timer = self.current_time
@@ -783,14 +783,14 @@ class IceShroom(Plant):
 
     def animation(self):
         if self.start_freeze:
-            if(self.current_time - self.animate_timer) > 500:
+            if(self.current_time - self.animate_timer) > 500 / c.DELTA_TIME:
                 self.frame_index += 1
                 if self.frame_index >= self.frame_num:
                     self.health = 0
                     return
                 self.animate_timer = self.current_time
         else:
-            if (self.current_time - self.animate_timer) > 100:
+            if (self.current_time - self.animate_timer) > 100 / c.DELTA_TIME:
                 self.frame_index += 1
                 if self.frame_index >= self.frame_num:
                     if self.state == c.SLEEP:
@@ -808,7 +808,7 @@ class HypnoShroom(Plant):
     def __init__(self, x, y):
         Plant.__init__(self, x, y, c.HYPNOSHROOM, 1, None)
         self.can_sleep = True
-        self.animate_interval = 200
+        self.animate_interval = 200 / c.DELTA_TIME
 
     def loadImages(self, name, scale):
         self.idle_frames = []
@@ -832,9 +832,9 @@ class WallNutBowling(Plant):
         self.level = level
         self.init_rect = self.rect.copy()
         self.rotate_degree = 0
-        self.animate_interval = 200
+        self.animate_interval = 200 / c.DELTA_TIME
         self.move_timer = 0
-        self.move_interval = 70
+        self.move_interval = 70 / c.DELTA_TIME
         self.vel_x = random.randint(12, 15)
         self.vel_y = 0
         self.disable_hit_y = -1
@@ -847,8 +847,8 @@ class WallNutBowling(Plant):
             self.move_timer = self.current_time
         elif (self.current_time - self.move_timer) >= self.move_interval:
             self.rotate_degree = (self.rotate_degree - 30) % 360
-            self.init_rect.x += self.vel_x
-            self.init_rect.y += self.vel_y
+            self.init_rect.x += self.vel_x * c.DELTA_TIME
+            self.init_rect.y += self.vel_y * c.DELTA_TIME
             self.handleMapYPosition()
             if self.shouldChangeDirection():
                 self.changeDirection(-1)
@@ -915,9 +915,9 @@ class RedWallNutBowling(Plant):
         self.explode_x_range = c.GRID_X_SIZE
         self.init_rect = self.rect.copy()
         self.rotate_degree = 0
-        self.animate_interval = 200
+        self.animate_interval = 200 / c.DELTA_TIME
         self.move_timer = 0
-        self.move_interval = 70
+        self.move_interval = 70 / c.DELTA_TIME
         self.vel_x = random.randint(12, 15)
 
     def loadImages(self, name, scale):
@@ -940,7 +940,7 @@ class RedWallNutBowling(Plant):
             self.move_timer = self.current_time
         elif (self.current_time - self.move_timer) >= self.move_interval:
             self.rotate_degree = (self.rotate_degree - 30) % 360
-            self.init_rect.x += self.vel_x
+            self.init_rect.x += self.vel_x * c.DELTA_TIME
             if self.init_rect.x > c.SCREEN_WIDTH:
                 self.health = 0
             self.move_timer += self.move_interval
@@ -949,7 +949,7 @@ class RedWallNutBowling(Plant):
         if self.explode_timer == 0:
             self.explode_timer = self.current_time
             self.changeFrames(self.explode_frames)
-        elif (self.current_time - self.explode_timer) > 500:
+        elif (self.current_time - self.explode_timer) > 500 / c.DELTA_TIME:
             self.health = 0
 
     def animation(self):
