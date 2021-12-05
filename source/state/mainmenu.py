1205b22fcd6d5c79faa4caa402742b3c666eaadf
Connor <jjh19960115@gmail.com>
2021-12-06 07:34:28 +0900

    Complete Main UI

--- source/state/mainmenu.py
+++ source/state/mainmenu.py
@@ -19,13 +19,21 @@ class Menu(tool.State):
         self.game_info = persist
         
         self.setupBackground()
+
         self.setupMuteSound()
         self.ClickedMuteSound()
         self.setupSound()
         self.ClickedSound()
+
+        self.setupEasyMode()
+        self.ClickedEasyMode()
+        self.setupNormalMode()
+        self.ClickedNormalMode()
+        self.setupHardMode()
+        self.ClickedHardMode()
+
         self.setupOption()
         
-
     def setupBackground(self):
         frame_rect = [80, 0, 800, 600]
         self.bg_image = tool.get_image(tool.GFX[c.MAIN_MENU_IMAGE], *frame_rect)
@@ -36,40 +44,85 @@ class Menu(tool.State):
         gameoff_rect = [0, 0 ,165 , 60]
         self.gameoff_img = tool.get_image(tool.GFX[c.OPTION_GAMEOFF],*gameoff_rect)
         self.gameoff_img_rect = self.gameoff_img.get_rect()
-        self.gameoff_img_rect.x = 460
-        self.gameoff_img_rect.y = 350
-        self.isclicked = False
+        self.gameoff_img_rect.x = 450
+        self.gameoff_img_rect.y = 340
+        self.gameOffclicked = False
 
     def setupMuteSound(self):
-        mutesound_rect = [0, 0, 72, 44]
+        mutesound_rect = [0, 0, 99, 55]
         self.mutesound_img = tool.get_image(tool.GFX[c.SOUND_MUTE_IMAGE],*mutesound_rect)
         self.mutesound_img_rect = self.mutesound_img.get_rect()
-        self.mutesound_img_rect.x = 460
-        self.mutesound_img_rect.y = 290
+        self.mutesound_img_rect.x = 440
+        self.mutesound_img_rect.y = 265
         self.muteSoundClicked = False
 
     def ClickedMuteSound(self):
-        clickedMutesound_rect = [0, 0, 72, 44]
+        clickedMutesound_rect = [0, 0, 99, 55]
         self.clickedMutesound_img = tool.get_image(tool.GFX[c.CLICKED_MUTE_IMAGE],*clickedMutesound_rect)
         self.clickedMutesound_img_rect = self.clickedMutesound_img.get_rect()
-        self.clickedMutesound_img_rect.x = 460
-        self.clickedMutesound_img_rect.y = 290
+        self.clickedMutesound_img_rect.x = 440
+        self.clickedMutesound_img_rect.y = 265
 
     def setupSound(self):
-        sound_rect = [0, 0, 72, 44]
+        sound_rect = [0, 0, 99, 55]
         self.sound_img = tool.get_image(tool.GFX[c.SOUND_IMAGE],*sound_rect)
         self.sound_img_rect = self.sound_img.get_rect()
-        self.sound_img_rect.x = 560
-        self.sound_img_rect.y = 290
+        self.sound_img_rect.x = 550
+        self.sound_img_rect.y = 275
         self.soundClicked = False
 
     def ClickedSound(self):
-        clickedsound_rect = [0, 0, 72, 44]
+        clickedsound_rect = [0, 0, 99, 55]
         self.clickedSound_img = tool.get_image(tool.GFX[c.CLICKED_SOUND_IMAGE],*clickedsound_rect)
         self.clickedSound_img_rect = self.clickedSound_img.get_rect()
-        self.clickedSound_img_rect.x = 560
-        self.clickedSound_img_rect.y = 290
+        self.clickedSound_img_rect.x = 550
+        self.clickedSound_img_rect.y = 275
         
+    def setupEasyMode(self):
+        easyMode_rect = [0, 0, 99, 55]
+        self.easyMode_img = tool.get_image(tool.GFX[c.EASY_IMAGE],*easyMode_rect)
+        self.easyMode_img_rect = self.easyMode_img.get_rect()
+        self.easyMode_img_rect.x = 410
+        self.easyMode_img_rect.y = 190
+        self.easyClicked = False
+
+    def ClickedEasyMode(self):
+        clickedEasy_rect = [0, 0, 99, 55]
+        self.clickedEasy_img = tool.get_image(tool.GFX[c.CLICKED_EASY],*clickedEasy_rect)
+        self.clickedEasy_img_rect = self.clickedEasy_img.get_rect()
+        self.clickedEasy_img_rect.x = 410
+        self.clickedEasy_img_rect.y = 190
+
+    def setupNormalMode(self):
+        normalMode_rect = [0, 0, 99, 55]
+        self.normalMode_img = tool.get_image(tool.GFX[c.NORMAL_IMAGE],*normalMode_rect)
+        self.normalMode_img_rect = self.normalMode_img.get_rect()
+        self.normalMode_img_rect.x = 510
+        self.normalMode_img_rect.y = 200
+        self.normalClicked = False
+
+    def ClickedNormalMode(self):
+        clickedNormal_rect = [0, 0, 99, 55]
+        self.clickedNormal_img = tool.get_image(tool.GFX[c.CLICKED_NORMAL],*clickedNormal_rect)
+        self.clickedNormal_img_rect = self.clickedNormal_img.get_rect()
+        self.clickedNormal_img_rect.x = 510
+        self.clickedNormal_img_rect.y = 200
+
+    def setupHardMode(self):
+        hardMode_rect = [0, 0, 99, 55]
+        self.hardMode_img = tool.get_image(tool.GFX[c.HARD_IMAGE],*hardMode_rect)
+        self.hardMode_img_rect = self.hardMode_img.get_rect()
+        self.hardMode_img_rect.x = 610
+        self.hardMode_img_rect.y = 210
+        self.hardClicked = False
+
+    def ClickedHardMode(self):
+        clickedHard_rect = [0, 0, 99, 55]
+        self.clickedHard_img = tool.get_image(tool.GFX[c.CLICKED_HARD],*clickedHard_rect)
+        self.clickedHard_img_rect = self.clickedHard_img.get_rect()
+        self.clickedHard_img_rect.x = 610
+        self.clickedHard_img_rect.y = 210
+
     def setupOption(self):
         self.option_frames = []
         frame_names = [c.OPTION_ADVENTURE + '_0', c.OPTION_ADVENTURE + '_1']
@@ -101,7 +154,7 @@ class Menu(tool.State):
         x, y = mouse_pos
         if(x >= self.gameoff_img_rect.x and x <= self.gameoff_img_rect.right and
            y >= self.gameoff_img_rect.y and y <= self.gameoff_img_rect.bottom):
-            self.isclicked = True
+            self.gameOffclicked = True
 
     def checkMuteSoundClick(self, mouse_pos):
         x, y = mouse_pos
@@ -109,16 +162,39 @@ class Menu(tool.State):
            y >= self.mutesound_img_rect.y and y <= self.mutesound_img_rect.bottom):
             self.muteSoundClicked = True
 
-            #self.start_sound.play()
-            #self.option_clicked = True
-            #self.option_timer = self.option_start = self.current_time
-        #return False
-
     def checkSoundClick(self, mouse_pos):
         x, y = mouse_pos
         if(x >= self.sound_img_rect.x and x <= self.sound_img_rect.right and
            y >= self.sound_img_rect.y and y <= self.sound_img_rect.bottom):
             self.soundClicked = True
+
+    def checkEasyClick(self, mouse_pos):
+        x, y = mouse_pos
+        if(x >= self.easyMode_img_rect.x and x <= self.easyMode_img_rect.right and
+           y >= self.easyMode_img_rect.y and y <= self.easyMode_img_rect.bottom):
+            self.easyClicked = True
+
+    def checkNormalClick(self, mouse_pos):
+        x, y = mouse_pos
+        if(x >= self.normalMode_img_rect.x and x <= self.normalMode_img_rect.right and
+           y >= self.normalMode_img_rect.y and y <= self.normalMode_img_rect.bottom):
+            self.normalClicked = True
+
+    def checkHardClick(self, mouse_pos):
+        x, y = mouse_pos
+        if(x >= self.hardMode_img_rect.x and x <= self.hardMode_img_rect.right and
+           y >= self.hardMode_img_rect.y and y <= self.hardMode_img_rect.bottom):
+            self.hardClicked = True
+
+    def easyConfig(self):
+        c.LOSTHEAD_HEALTH = 3
+
+    def normalConfig(self):
+        c.LOSTHEAD_HEALTH = 5
+    
+    def hardConfig(self):
+        c.LOSTHEAD_HEALTH = 7
+
 
     def update(self, surface, current_time, mouse_pos, mouse_click):
         self.current_time = self.game_info[c.CURRENT_TIME] = current_time
@@ -129,6 +205,9 @@ class Menu(tool.State):
                 self.checkGameOffClick(mouse_pos)
                 self.checkSoundClick(mouse_pos)
                 self.checkMuteSoundClick(mouse_pos)
+                self.checkEasyClick(mouse_pos)
+                self.checkNormalClick(mouse_pos)
+                self.checkHardClick(mouse_pos)
         else:
             if(self.current_time - self.option_timer) > 200:
                 self.option_frame_index += 1
@@ -146,28 +225,47 @@ class Menu(tool.State):
 
         surface.blit(self.bg_image, self.bg_rect)
         surface.blit(self.option_image, self.option_rect)
-
-        if(self.isclicked == False):
+        
+        #게임 종료 
+        if(self.gameOffclicked == False):
             surface.blit(self.gameoff_img,self.gameoff_img_rect)
         else:
-            pg.quit()
-
-
-        if(self.muteSoundClicked == False):
-            surface.blit(self.mutesound_img, self.mutesound_img_rect)
-            if(self.soundClicked == True):
-                self.setupSound()         
-        else:
-            surface.blit(self.clickedMutesound_img, self.clickedMutesound_img_rect)
-            pg.mixer.music.pause()
-
-        if(self.soundClicked == False):
+            pg.quit()            
+        
+        #소리 설정
+        if(self.muteSoundClicked == False and self.soundClicked == False):
             surface.blit(self.sound_img, self.sound_img_rect)
-        else:
+            surface.blit(self.mutesound_img, self.mutesound_img_rect)
+        elif(self.muteSoundClicked == True and self.soundClicked == False):
+            surface.blit(self.clickedMutesound_img, self.clickedMutesound_img_rect)
+            surface.blit(self.sound_img, self.sound_img_rect)
+            pg.mixer.music.pause()
+            self.muteSoundClicked = False
+        elif(self.muteSoundClicked == False and self.soundClicked == True):
             surface.blit(self.clickedSound_img, self.clickedSound_img_rect)
+            surface.blit(self.mutesound_img, self.mutesound_img_rect)
             pg.mixer.music.unpause()
-            self.setupMuteSound()
-            #self.checkMuteSoundClick(mouse_pos)
+            self.soundClicked = False
+
+        #난이도 설정
+        if(self.easyClicked == False):
+            surface.blit(self.easyMode_img, self.easyMode_img_rect)
+        else:
+            surface.blit(self.clickedEasy_img, self.clickedEasy_img_rect)
+            self.easyConfig()
+
+        if(self.normalClicked == False):
+            surface.blit(self.normalMode_img, self.normalMode_img_rect)
+        else:
+            surface.blit(self.clickedNormal_img, self.clickedNormal_img_rect)
+            self.normalConfig()
+
+        if(self.hardClicked == False):
+            surface.blit(self.hardMode_img, self.hardMode_img_rect)
+        else:
+            surface.blit(self.clickedHard_img, self.clickedHard_img_rect)
+            self.hardConfig()
+
 
         
         
